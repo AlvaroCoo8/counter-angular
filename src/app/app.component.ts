72bdb91ee2d549f649f31b93ac5a07bf6ecf1876
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2025-01-22 08:57:24 +0100

    counter funtional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,17 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'counter';
+  public title: string = 'Counter implemented in Angular';
+  public counter: number = 0;
+
+  public increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  public reset(value: number): void {
+    this.counter = 0;
+  }
 }
